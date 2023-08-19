5b1f1b70f502bae8203a33a7929251ee04308876
Yohikofox <yohikofox360@live.fr>
2023-08-19 18:26:31 +0200

    Add offline data checks

--- src/business/useCases/getOfflinePageData.ts
+++ src/business/useCases/getOfflinePageData.ts
@@ -5,9 +5,11 @@ import OffLinePageData from "../model/offLinePageData";
 
 export interface GetOfflinePageDataRequest { }
 
+
 export enum GetOfflinePageDataUseCaseResult {
   SUCCESS = 'success',
   ERROR = 'error',
+  NO_DATA_FOUND = "NO_DATA_FOUND"
 }
 
 export default class GetOfflinePageDataUseCase implements IUseCase<GetOfflinePageDataRequest, Result<OffLinePageData, GetOfflinePageDataUseCaseResult>> {
@@ -18,6 +20,10 @@ export default class GetOfflinePageDataUseCase implements IUseCase<GetOfflinePag
 
     if (response.IsError) {
       return response.transferError(GetOfflinePageDataUseCaseResult.ERROR)
+    }
+
+    if (!response.Value.offlinePage.data){
+      return response.transferError(GetOfflinePageDataUseCaseResult.NO_DATA_FOUND)
     }
 
     const result: OffLinePageData = {
