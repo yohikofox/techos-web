8637ab2ecdc4e9b84410854ece94d668bda15084
Yohikofox <yohikofox360@live.fr>
2023-12-11 11:54:57 +0100

    Comment Redis references in API

--- src/app/(admin)/api/admin/cache/tags/[id]/route.ts
+++ src/app/(admin)/api/admin/cache/tags/[id]/route.ts
@@ -3,17 +3,21 @@ import RedisCacheHandler from 'R/src/infrastructure/cache/redis/redis-cache-hand
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const Provider = CacheFactory.resolve()
-  const cache = await new Provider({}).get(params.id);
+  // const Provider = CacheFactory.resolve()
+  // const cache = await new Provider({}).get(params.id);
 
-  if (!cache) return NextResponse.json("Not found", { status: 404 });
+  // if (!cache) return NextResponse.json("Not found", { status: 404 });
 
-  return NextResponse.json({ ...cache });
+  // return NextResponse.json({ ...cache });
+
+  return NextResponse.json({ success: true });
 }
 
 
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-  const Provider = CacheFactory.resolve()
-  await new Provider({}).remove(params.id);
+  // const Provider = CacheFactory.resolve()
+  // await new Provider({}).remove(params.id);
+  // return NextResponse.json({ success: true });
+
   return NextResponse.json({ success: true });
 }
--- src/app/(admin)/api/admin/cache/tags/route.ts
+++ src/app/(admin)/api/admin/cache/tags/route.ts
@@ -2,11 +2,13 @@ import CacheFactory from 'R/src/infrastructure/cache/CacheFactory';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  const Provider = CacheFactory.resolve()
+  // const Provider = CacheFactory.resolve()
 
-  const cacheProvider = new Provider({})
+  // const cacheProvider = new Provider({})
 
-  const cache = await cacheProvider.list<string>();
+  // const cache = await cacheProvider.list<string>();
 
-  return NextResponse.json({ items: cache });
+  // return NextResponse.json({ items: cache });
+
+  return NextResponse.json({ items: [] });
 }
