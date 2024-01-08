e78c2f318edae6b969cb34501dcf01f4c2e65a38
Yohikofox <yohikofox360@live.fr>
2024-01-08 16:27:58 +0100

    Add revalidate > 0 + tags

--- src/lib/constants/revalidateTag.ts
+++ src/lib/constants/revalidateTag.ts
@@ -2,4 +2,5 @@ export default class RevalidateTagConstants {
   static readonly POST = 'post';
   static readonly HOME = 'home';
   static readonly HEADER_DATA = 'header_data';
+  static readonly CONFIG = 'config';
 }
