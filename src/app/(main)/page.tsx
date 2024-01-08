b7a6ac6158f3a06d75ba962dd3390ee855f4dc6d
Yohikofox <yohikofox360@live.fr>
2024-01-08 17:53:43 +0100

    rollback previous commit

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,9 @@
-// import PostList from "./liste-de-posts/[page]/page"
+import PostList from "./liste-de-posts/[page]/page"
 
 export default async function Home(context: any) {
   return (
     <>
-      {/* <PostList {...context} /> */}
+      <PostList {...context} />
     </>
   )
 }
