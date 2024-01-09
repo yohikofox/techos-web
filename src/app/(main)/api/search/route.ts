62d7cb62dc57357249eb558fa35949f2c2780681
Yohikofox <yohikofox360@live.fr>
2024-01-09 14:15:58 +0100

    remove log

--- src/app/(main)/api/search/route.ts
+++ src/app/(main)/api/search/route.ts
@@ -9,8 +9,6 @@ export async function GET(request: NextRequest) {
 
   const payload = request.nextUrl.searchParams.get("payload");
 
-  console.log("🚀 ~ file: route.ts:11 ~ GET ~ payload:", payload)
-
   if (!payload) {
     return badRequest();
   }
