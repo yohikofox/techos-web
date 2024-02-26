3d47cc55e0b51943bcdd38450b002f98ce8657b0
Yohikofox <yohikofox360@live.fr>
2024-02-26 22:48:10 +0100

    Ajouter un suspense pour le chargement des facettes

--- src/app/(main)/components/PostList/parts/FacetCollection/_parts/SimpleFacet/index.tsx
+++ src/app/(main)/components/PostList/parts/FacetCollection/_parts/SimpleFacet/index.tsx
@@ -1,4 +1,5 @@
 import { FacetedSearch, FacetedValue } from "R/src/domain/search";
+import { Suspense } from "react";
 
 import FacetItem from "../FacetItem";
 import styles from "./styles.module.scss";
@@ -10,13 +11,15 @@ export default function Component({ facet }: { facet: FacetedSearch }) {
         <h4>{facet.label}</h4>
         {facet.values.map((value: FacetedValue, index: number) => {
           return (
-            <FacetItem
-              key={index}
-              className={styles.item}
-              data={{ ...value, key: facet.name }}
-              multiple={facet.multiple}
-              autocomplete={facet.autocomplete}
-            />
+            <Suspense key={index} fallback={<div>Loading...</div>}>
+              <FacetItem
+                key={index}
+                className={styles.item}
+                data={{ ...value, key: facet.name }}
+                multiple={facet.multiple}
+                autocomplete={facet.autocomplete}
+              />
+            </Suspense>
           );
         })}
       </section>
